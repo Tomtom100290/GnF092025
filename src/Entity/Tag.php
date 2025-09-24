f2ad49f4b503c2617cedc09c3f22eacd67f7b2e3
Thomy NOEL <ThomyN@domurc.urcoopa.fr>
2025-09-24 12:10:42 +0400

    entité tag

---
+++ src/Entity/Tag.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $codeTag = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCodeTag(): ?string
+    {
+        return $this->codeTag;
+    }
+
+    public function setCodeTag(?string $codeTag): static
+    {
+        $this->codeTag = $codeTag;
+
+        return $this;
+    }
+}
