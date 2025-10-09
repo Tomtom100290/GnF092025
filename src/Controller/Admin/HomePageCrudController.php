6ea70db13c7276dc23b11ce30ec774b9aa089cc4
Thomy NOEL <ThomyN@domurc.urcoopa.fr>
2025-10-09 14:59:02 +0400

    Configuration page HomeCrudController du dash board

--- src/Controller/Admin/HomePageCrudController.php
+++ src/Controller/Admin/HomePageCrudController.php
@@ -9,6 +9,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+//Pour modifier les actions comme "Editer", "Supprimer"..
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+//Pour Parametrer l'affichage d'une page du dashboard
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
 
 class HomePageCrudController extends AbstractCrudController
 {
@@ -21,11 +27,33 @@ class HomePageCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('h1'),
-            TextField::new('h2'),
-            TextareaField::new('paragraphe'),
-            ImageField::new('img')->setUploadDir('assets/images/'),
+            IdField::new('h1')->setLabel('Titre'),
+            TextField::new('h2')->setLabel('Sous-Titre'),
+            TextareaField::new('paragraphe')->setLabel('Présentation'),
+            ImageField::new('img')->setUploadDir('assets/images/')->setLabel('Illustration'),
         ];
     }
+    // Pour désactiver l'option supprimer dans le dash bord dans 
+    //la configuration de la partie entête de la page d'accueil
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+            return $action->setLabel('Modifier la page'); //Modifie le libellé du bouton Editer
+        })
+       
+           
+            ->disable(Action::DELETE, Action::NEW); // Désactive le bouton Supprimer
+    }
+
+    // Paramettrage de la page , titre par exemple
+    public function configureCrud(Crud $crud): Crud
+{
+    return $crud
+    //Parametrage de l'affichage listing, edition, details
+        ->setPageTitle(Crud::PAGE_INDEX, 'Parametrage entête page d\'Accueil')      // titre de la page listing
+        ->setPageTitle(Crud::PAGE_EDIT, 'Modifier l\'entête') // titre de la page édition
+        ->setPageTitle(Crud::PAGE_DETAIL, 'Détail de l\'entête'); // si tu as activé la vue détail
+}
     
 }
