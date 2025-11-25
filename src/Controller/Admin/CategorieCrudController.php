145fa2070924de2179321b9a64a926b07f1b9040
Thomy NOEL <ThomyN@domurc.urcoopa.fr>
2025-11-25 16:26:37 +0400

    Mise a jour fichier css+ creation base_auth.html.twig

--- src/Controller/Admin/CategorieCrudController.php
+++ src/Controller/Admin/CategorieCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categorie;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -17,7 +20,34 @@ class CategorieCrudController extends AbstractCrudController
         return Categorie::class;
     }
 
-    
+     public function configureActions(Actions $actions): Actions
+    {
+
+        return $actions
+            // Modifier le texte du bouton "Save" sur la page EDIT
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+                return $action->setLabel('Enregistrer et retour');
+            })
+
+            // Mettre "EDIT" dans les trois petits points
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action->setLabel('Modifier');
+            })
+
+            // Même chose pour DELETE si tu veux
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                return $action->setLabel('Supprimer');
+            })
+            // Modifier le texte du bouton "New"
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->setLabel('Ajouter une Catégorie');
+            })
+            // Ordre dans la colonne Actions
+            ->reorder(Crud::PAGE_INDEX, [
+                Action::EDIT,
+                Action::DELETE,
+            ]);
+    }
     public function configureFields(string $pageName): iterable
     {
         return [
